b38f52649e59588a25acae984e3b39a5ec53fb55
jmjf <jamee.mikell@gmail.com>
2022-02-23 03:25:50 +0000

    fix log messages and body ref

--- AzFunctions/post-requests/index.ts
+++ AzFunctions/post-requests/index.ts
@@ -9,19 +9,19 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const fnName = 'requests';
     const supportedApiVersions = ['2022-02-12', '2022-02-15'];
     let httpStatus = 0;
-    context.log(`| INFO | ${fnName} | received request`);
+    context.log(`INFO | ${fnName} | received request`);
 
     // request validation
     if (!req.body) {
-        context.log(`| ERROR | ${fnName} | req.body falsey`);
+        context.log(`ERROR | ${fnName} | req.body falsey`);
         httpStatus = 400;
     } else if (!req.body.apiVersion) {
-        context.log(`| ERROR | ${fnName} | req.body.apiVersion falsey`);
+        context.log(`ERROR | ${fnName} | req.body.apiVersion falsey`);
         httpStatus = 400;
     } else {
         // we have a body (data payload) and API version
         if (!supportedApiVersions.includes(req.body.apiVersion)) {
-            context.log(`| ERROR | ${fnName} | invalid apiVersion ${req.body.apiVersion}`);
+            context.log(`ERROR | ${fnName} | invalid apiVersion ${req.body.apiVersion}`);
             httpStatus = 400;
         } else {
             // common to all supported API versions
@@ -36,7 +36,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
                 if (!req.body.only0212) {
                     context.log(`INFO | ${fnName} | apiVersion 2022-02-12 missing optional only0212`);
                 } else {
-                    context.log(`INFO | ${fnName} | apiVersion 2022-02-12 has optional only0212 ${req.body.only021}`)
+                    context.log(`INFO | ${fnName} | apiVersion 2022-02-12 has optional only0212 ${req.body.only0212}`)
                 }
             } else if (req.body.apiVersion == '2022-02-15') {
                 // Check request components for API version 2022-02-15 if needed
